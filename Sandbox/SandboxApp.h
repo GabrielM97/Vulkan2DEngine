b0227a3242abe029265a276fd7d274d2ab9c8c96
Aleksandra <a.petkova1617@gmail.com>
2026-04-12 15:01:43 +0100

    add hierarchy and inspector ImGui panels Optional body: Add a hierarchy panel that renders the scene tree and tracks the selected game object.
    Add an inspector panel that shows the selected object's name and ID.

--- Sandbox/SandboxApp.h
+++ Sandbox/SandboxApp.h
@@ -3,6 +3,7 @@
 #include "Core/Application.h"
 #include "Scene/Scene.h"
 
+#include <array>
 #include <vector>
 #include <imgui.h>
 
@@ -16,6 +17,20 @@ protected:
     void OnImGuiUpdate() override;
 
 private:
+    void SelectObject(GameObjectID id);
+    void SyncInspectorFromSelection();
+    void DrawHierarchyPanel();
+    void DrawHierarchyNode(const GameObject& object);
+    void DrawInspectorPanel();
+
     Scene m_Scene;
     GameObjectHandle m_Player;
+    GameObjectID m_SelectedObjectID = 0;
+    GameObjectID m_InspectorObjectID = 0;
+
+    std::array<char, 256> m_NameBuffer{};
+    std::array<char, 260> m_TexturePathBuffer{};
+    int m_ReparentTargetID = 0;
+    int m_SourceRectValues[4] = {0, 0, 0, 0};
+    int m_SourceGridValues[4] = {0, 0, 64, 64};
 };
